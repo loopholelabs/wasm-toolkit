453caddbf193afe560fecbf359e440ce1b3ae7a5
Jimmy Moore <jamesmoore@loopholelabs.io>
2023-04-14 23:18:43 +0100

    Added run.sh

---
+++ cmd/run.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+rm module1_strace.wasm
+
+go run . strace -i ../module1.wasm -o module1_strace.wasm --all --color --func '^\$IMPORT.*|\_start' --timing true
+
+wasmtime --dir . --env TEST=1 module1_strace.wasm 1 2 3
